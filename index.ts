279ac365a8368cedbfbc86d44ca2350dbde59373
James Aprosail <aprosail@outlook.com>
2024-06-23 20:07:08 +0800

    fix punctuation for zh,jp

--- index.ts
+++ index.ts
@@ -6,9 +6,6 @@ import {langIndexOf} from "./data"
 
 export * from "./data"
 
-/** A space, to optimize readabilities, compare to empty string. */
-const space = " "
-
 /**
  * A markdown-it plugin to optimize wordless multi-language line-break render.
  * See [readme](./README.md) of this package for more details.
@@ -42,21 +39,35 @@ const space = " "
  */
 export function wordless(md: md, options?: Options) {
   md.renderer.rules.softbreak = function (tokens, index) {
-    if (index === 0 || index === tokens.length - 1) return space
+    if (index === 0 || index === tokens.length - 1) return " "
     const prefix = tokens[index - 1].content
     const suffix = tokens[index + 1].content
     const before = langIndexOf(prefix.charCodeAt(prefix.length - 1), options)
     const after = langIndexOf(suffix.charCodeAt(0), options)
-    return before === after && before !== -1 && before != -2 ? "" : space
+
+    if (before === after) return "" // Same wordless language.
+    if (before === -3 || after === -3) return "" // Special punctuations.
+    if ((before === -2 && after >= 0) || (after === -2 && before >= 0))
+      return "" // Resolve emoji.
+
+    return " "
   }
 }
 
 if (import.meta.vitest) {
   const {expect, test} = import.meta.vitest
+
   test("basic function", function () {
     const raw = "English\nにほんご\n中文\n中文\nབོད་ཡིག།\nབོད་ཡིག།"
     expect(new MarkdownIt().use(wordless).render(raw)).toBe(
       "<p>English にほんご中文中文 བོད་ཡིག།བོད་ཡིག།</p>\n",
+    )
+  })
+
+  test("zh,jp punctuations", function () {
+    const raw = "全角符号，\n全角記号。\nぜんかくきごう"
+    expect(new MarkdownIt().use(wordless).render(raw)).toBe(
+      "<p>全角符号，全角記号。ぜんかくきごう</p>\n",
     )
   })
 }
