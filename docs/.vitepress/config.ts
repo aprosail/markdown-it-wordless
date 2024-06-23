247e316f418b3ced96caf9dc9389b3499a92384e
James Aprosail <aprosail@outlook.com>
2024-06-23 18:51:52 +0800

    vitepress docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,13 @@
+import {defineConfig} from "vitepress"
+
+export default defineConfig({
+  title: "Markdown-it Wordless",
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: "github",
+        link: "https://github.com/treeinfra/markdown-it-wordless",
+      },
+    ],
+  },
+})
